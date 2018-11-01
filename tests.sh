c3cab7a6a6e01357b8a2dc5ddb1bd11b49f245b6
sand-enqos <sandeepp@enqos.com>
2018-11-01 21:40:11 +0530

    kill the port 3000

--- tests.sh
+++ tests.sh
@@ -7,6 +7,7 @@ docker-compose up --timeout 1 --no-build -d
 rm -rf nohup
 nohup docker-compose run tests
 errorMessage="Test failed.  See above for more details."
+sudo kill `sudo lsof -t -i:3000`
 if grep -q  "$errorMessage" "nohup.out"; then
     echo "Tests Failed"
     exit 1
@@ -14,6 +15,7 @@ else
     echo "Tests Passed"
     docker-compose stop
     docker-compose rm -f
+    sudo kill `sudo lsof -t -i:3000`
     npm start
 fi
 
