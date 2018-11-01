6efff7924e3ed4cadfd774b3c3bfd50ae1ede2f4
sand-enqos <sandeepp@enqos.com>
2018-11-01 21:36:33 +0530

    tests in shell

---
+++ shell.sh
@@ -0,0 +1,1 @@
+echo "Shell for test"
--- tests.sh
+++ tests.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-# integration-tests.sh
 echo "tests form shell..."
-res1=$(nohup npm test --watchAll)
-echo $res1;
-echo "$res1";
+docker-compose stop
+docker-compose rm -f
+docker-compose build
+docker-compose up --timeout 1 --no-build -d
+rm -rf nohup
+nohup docker-compose run tests
+errorMessage="Test failed.  See above for more details."
+if grep -q  "$errorMessage" "nohup.out"; then
+    echo "Tests Failed"
+    exit 1
+else
+    echo "Tests Passed"
+    docker-compose stop
+    docker-compose rm -f
+    npm start
+fi
+
+
+
+
 
 
